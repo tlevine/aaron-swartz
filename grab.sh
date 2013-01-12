765af2bf45f52f8f3d7493e56bb61e965a3d6086
Thomas Levine <occurrence@thomaslevine.com>
2013-01-12 14:14:40 -0500

    aaronsw.jottit.com

--- grab.sh
+++ grab.sh
@@ -4,4 +4,5 @@ alias grab='wget -e robots=off -r -l inf -pk'
 
 # grab http://qblog.aaronsw.com/
 # grab http://www.aaronsw.com/
-grab https://pinboard.in/u:aaronsw
+# grab https://pinboard.in/u:aaronsw
+grab https://aaronsw.jottit.com/ --no-check-certificate
